97c85f227191c2b33371f22bd615357c832d4744
dataluchs <tim_stehle@posteo.de>
2021-09-17 16:44:15 +0200

    add sendgrid to send mail when domain not on client hold anymore

--- api/whois.js
+++ api/whois.js
@@ -1,36 +1,61 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-router.get('/', async (req, res) => {
-    
-    try {
+router.get("/", async (req, res) => {
+  try {
+    const API_KEY = process.env.API_KEY;
+    const DOMAIN_NAME = process.env.DOMAIN_NAME;
+    const CREDITS = process.env.CREDITS;
+    const OUTPUT_FORMAT = process.env.OUTPUT_FORMAT;
+    const SEND_TO_EMAIL = process.env.SEND_TO_EMAIL;
+    const SEND_FROM_EMAIL = process.env.SEND_FROM_EMAIL;
 
-        const API_KEY = process.env.API_KEY
-        const DOMAIN_NAME = process.env.DOMAIN_NAME
-        const CREDITS = process.env.CREDITS
-        const OUTPUT_FORMAT = process.env.OUTPUT_FORMAT
+    const uri = encodeURI(
+      `https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=${API_KEY}&domainName=${DOMAIN_NAME}&credits=${CREDITS}&outputFormat=${OUTPUT_FORMAT}`
+    );
 
-        const uri = encodeURI(
-            `https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=${API_KEY}&domainName=${DOMAIN_NAME}&credits=${CREDITS}&outputFormat=${OUTPUT_FORMAT}`
-        )
+    const headers = {
+      "Content-Type": "application/json",
+    };
 
-        const headers = {
-                'Content-Type': 'application/json',
+    const response = await axios.get(uri, headers, { withCredentials: true });
+
+    const whoisResponse = response.data;
+
+    const domainName = whoisResponse.WhoisRecord.domainName;
+    const domainStatus = whoisResponse.WhoisRecord.registryData.status;
+    let checkFor = "clientHold";
+
+    if (domainStatus.includes(checkFor)) {
+      console.log("Domain status is still on client hold.");
+    } else {
+      console.log("Domain status is not on client hold anymore!");
+      const msg = {
+        to: SEND_TO_EMAIL,
+        from: SEND_FROM_EMAIL, // Use the email address or domain you verified above
+        subject: `Domain ${domainName} NOT ON CLIENT HOLD ANYMORE!`,
+        text: "Domain not on client hold anymore, lets buy the domain, friend :)",
+        html: "<strong>Domain not on client hold anymore, lets buy the domain, friend :)</strong>",
+      };
+      (async () => {
+        try {
+          await sgMail.send(msg);
+        } catch (error) {
+          console.error(error);
+
+          if (error.response) {
+            console.error(error.response.body);
+          }
         }
-        
-        const response = await axios.get(
-            uri, 
-            headers,
-            { withCredentials: true })
-        return res.status(200).json(response.data)
-
-    } 
-    
-    catch (err) {
-        console.error(err.message)
-        return res.status(400).json({ msg: 'Bad request.' })
+      })();
     }
-})
+  } catch (err) {
+    console.error(err.message);
+    return res.status(400).json({ msg: "Bad request." });
+  }
+});
 
-module.exports = router
+module.exports = router;
